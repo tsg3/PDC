d7e0d99f52612b2824f4c49750018a2273fa731b
Esteban <estebandcg1999@gmail.com>
2019-05-06 01:34:25 -0600

    'PDC-Sol' y 'PDC-Todas'. Falta acomodar codigo y buscar formas de optimizar la solucion encontrada.

--- PDC.rkt
+++ PDC.rkt
@@ -21,6 +21,10 @@
         )
   )
 
+(define (num size casilla)
+  (+ (* size (car casilla)) (cadr casilla) 1)
+  )
+
 ;;; list_to_par_orden
 ;;; convierten una lista de casillas a una lista de pares ordenados
 
@@ -30,6 +34,16 @@
         '())
     (else
         (cons (par_orden size (car lista)) (list_to_par_orden size (cdr lista)))
+        )
+    )
+  )
+
+(define (list_to_num size lista)
+  (cond
+    ((null? lista)
+        '())
+    (else
+        (cons (num size (car lista)) (list_to_num size (cdr lista)))
         )
     )
   )
@@ -79,8 +93,8 @@
 ;;; crear_tablero
 ;;; crea el grafo que relaciona a las casillas del tablero
 
-(define (crear_tablero size)
-  (crear_grafo size (list_to_par_orden size (crear_tablero_aux (* size size) 1)))
+(define (crear_tablero size casilla una_solucion)
+  (buscar_ruta casilla (* size size) (crear_grafo size (list_to_par_orden size (crear_tablero_aux (* size size) 1))) una_solucion)
   )
 
 (define (crear_tablero_aux size casilla)
@@ -102,4 +116,198 @@
         )
     )
     )
+
+;;
+;;
+
+(define g '(((0 0) ((1 2) (2 1)))
+ ((0 1) ((1 3) (2 2) (2 0)))
+ ((0 2) ((1 4) (1 0) (2 3) (2 1)))
+ ((0 3) ((1 1) (2 4) (2 2)))
+ ((0 4) ((1 2) (2 3)))
+ ((1 0) ((2 2) (0 2) (3 1)))
+ ((1 1) ((2 3) (0 3) (3 2) (3 0)))
+ ((1 2) ((2 4) (2 0) (0 4) (0 0) (3 3) (3 1)))
+ ((1 3) ((2 1) (0 1) (3 4) (3 2)))
+ ((1 4) ((2 2) (0 2) (3 3)))
+ ((2 0) ((3 2) (1 2) (4 1) (0 1)))
+ ((2 1) ((3 3) (1 3) (4 2) (4 0) (0 2) (0 0)))
+ ((2 2) ((3 4) (3 0) (1 4) (1 0) (4 3) (4 1) (0 3) (0 1)))
+ ((2 3) ((3 1) (1 1) (4 4) (4 2) (0 4) (0 2)))
+ ((2 4) ((3 2) (1 2) (4 3) (0 3)))
+ ((3 0) ((4 2) (2 2) (1 1)))
+ ((3 1) ((4 3) (2 3) (1 2) (1 0)))
+ ((3 2) ((4 4) (4 0) (2 4) (2 0) (1 3) (1 1)))
+ ((3 3) ((4 1) (2 1) (1 4) (1 2)))
+ ((3 4) ((4 2) (2 2) (1 3)))
+ ((4 0) ((3 2) (2 1)))
+ ((4 1) ((3 3) (2 2) (2 0)))
+ ((4 2) ((3 4) (3 0) (2 3) (2 1)))
+ ((4 3) ((3 1) (2 4) (2 2)))
+ ((4 4) ((3 2) (2 3)))))
+
+;;;
+;;;
+
+(define (eliminar ele list)
+  (cond
+    ( (equal? ele (car list)) (cdr list) )
+    ( (null? (cdr list)) list)
+    ( else (cons (car list) (eliminar ele (cdr list))) )
+    )
+  )
+
+(define (append-element lst elem)
+  (cond
+    ((null? lst)
+        elem)
+    ((list? (car lst))
+        (append lst (list elem)))
+    (else
+        (append (list lst) (list elem)))
+    )
+  )
+
+(define (miembro ele list)
+  (cond ( (null? list) '() )
+        ( (equal? (car list) ele ) ele)
+        (else (miembro ele (cdr list)) )
+    )
+  )
+
+(define (obtener_vecinos ele graph)
+  (cond ( (null? graph) '() )
+        ( (equal? (caar graph) ele ) (cadar graph))
+        (else (obtener_vecinos ele (cdr graph)) )
+    )
+  )
+
+;;;
+;;;---------------------------------------------------------------------------------------------------------
+;;;
+;;;
+
+(define (buscar_ruta u limite grafo una_solucion)
+  (buscar_ruta_aux 1 u u limite grafo una_solucion '())
+  )
+
+(define (buscar_ruta_aux n ruta nodo limite grafo una_solucion rutas)
+  (cond
+    ((< n limite)
+        (buscar_nueva_ruta n ruta nodo limite (obtener_vecinos nodo grafo) grafo una_solucion rutas))
+    (else
+        (list_to_num (sqrt limite) ruta))
+        ;ruta)
+    )
+  )
+
+;(PDC-Sol 5 '(0 0))(PDC-Todas 5 '(0 0))
+
+(define (buscar_nueva_ruta n ruta nodo limite vecinos grafo una_solucion rutas)
+  (buscar_nueva_ruta_aux n ruta nodo limite vecinos grafo una_solucion rutas)
+  )
+
+(define (listar lst)
+  (cond
+    ((equal? lst #f)
+        '())
+    (else
+        lst)
+    )
+  )
+
+(define (unir lst1 lst2)
+  (cond
+    ((null? lst1)
+        lst2)
+    ((null? lst2)
+        lst1)
+    ((and (null? lst1) (null? lst2))
+        '())
+    ((and (not (list? (car lst1))) (not (list? (car lst2))))
+        (list lst1 lst2))
+    ((not (list? (car lst1)))
+        (cons lst1 lst2))
+    ((not (list? (car lst2)))
+        (append lst1 (list lst2)))
+    (else
+        (append lst1 lst2))
+    )
+  )
+
+(define (buscar_nueva_ruta_aux n ruta nodo limite vecinos grafo una_solucion rutas)
+  (cond
+    ((null? vecinos)
+        #f)
+    ((null? (miembro (car vecinos) ruta))
+      (cond
+        (una_solucion 
+            (verificar_ruta n ruta nodo limite vecinos (buscar_ruta_aux (+ n 1) (append-element ruta (car vecinos)) (car vecinos) limite grafo una_solucion rutas) grafo una_solucion rutas))
+        (else
+            (unir (listar(verificar_ruta n ruta nodo limite vecinos (buscar_ruta_aux (+ n 1) (append-element ruta (car vecinos)) (car vecinos) limite grafo una_solucion rutas) grafo una_solucion rutas)) (listar(buscar_nueva_ruta_aux n ruta nodo limite (cdr vecinos) grafo una_solucion rutas))))))
+    (else
+        (buscar_nueva_ruta_aux n ruta nodo limite (cdr vecinos) grafo una_solucion rutas))
+    )
+  )
+
+(define (last-element list)
+  (cond
+    ((null? (cdr list))
+        '())
+    ((null? (cddr list))
+        (car list))
+    (else
+        (last-element (cdr list)))
+    )
+  )
+
+(define (verificar_ruta n ruta nodo limite vecinos ruta2 grafo una_solucion rutas)
+  (cond
+    ((not ruta2)
+        (buscar_nueva_ruta_aux n ruta nodo limite (cdr vecinos) grafo una_solucion rutas))
+    ((not una_solucion)
+        ruta2)
+    (else
+        ruta2)
+    )
+  )
+
+;;;
+;;;-----------------------------------------------------------------------------------------------------------
+;;;
+;;;
+
+(define (PDC-Sol size casilla)
+  (cond
+    ((verificar_casilla size casilla)
+        (crear_tablero size casilla #t))
+    (else
+        #f)
+    )
+  )
+
+(define (PDC-Todas size casilla)
+  (cond
+    ((verificar_casilla size casilla)
+        (crear_tablero size casilla #f))
+    (else
+        #f)
+    )
+  )
+
+(define (verificar_casilla size casilla)
+  (cond
+    ((and (>= (car casilla) 0) (< (car casilla) size) (>= (cadr casilla) 0) (< (cadr casilla) size))
+        #t)
+    (else
+        #f)
+    )
+  )
+
+
+
+
+
+
+
 
